5e8a8a60d2d187e4bb3a0484c7cff63c2ffc8672
Lirido1404 <102609041+Lirido1404@users.noreply.github.com>
2024-02-11 23:31:24 +0100

    2nd commit

--- app.js
+++ app.js
@@ -4,3 +4,25 @@ const app = express();
 
 
 app.listen(3000);
+
+
+app.get('/',(req,res)=>{
+   // res.send('<p>home</p>')
+   res.sendFile('./views/index.html',{root: __dirname});
+})
+
+
+app.get('/about',(req,res)=>{
+ //   res.send('<p>About</p>')
+ res.sendFile('./views/about.html',{root: __dirname});
+})
+
+
+app.get('/about-us',(req,res)=>{
+    res.redirect('/about');
+})
+
+app.use((req,res)=>{
+    res.sendFile('./views/404.html',{root: __dirname});
+})
+
